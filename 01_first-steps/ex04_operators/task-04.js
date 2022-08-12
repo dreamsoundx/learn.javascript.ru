8dcb0d605cc3a4849277773eb5cd546b2bc93582
DreamsoundX <itsdreamsound@gmail.com>
2022-08-12 18:02:25 +0300

    task-04 is done

--- 01_first-steps/ex04_operators/task-04.js
+++ 01_first-steps/ex04_operators/task-04.js
@@ -3,13 +3,13 @@ let b = prompt('Второе число?', 2);
 
 alert(a + b); // 12
 
-alert(+a + +b);
+alert(+a + +b); // 3
 
 // или
 
-let a = +prompt('Первое число?', 1);
-let b = +prompt('Второе число?', 2);
+// let a = +prompt('Первое число?', 1);
+// let b = +prompt('Второе число?', 2);
 
-alert(a + b); // 12
+// alert(a + b); // 12
 
-alert(a + b);
+// alert(a + b); // 3
